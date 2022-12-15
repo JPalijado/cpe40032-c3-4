a12290ea1c950e869b42f41b1211bdd3b67f9dd2
John Emmanuel Palijado <palijadojohnemmanuel@gmail.com>
2022-12-16 00:25:27 +0800

    initial commit
    sticks to the challenge number 3 controls and game objects

---
+++ cpe40032-c3-4/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Problem 2 (The background should move at start, then stop when the game is over)
+        // Solution: modify condition from playerControllerScript.gameOver to playerControllerScript.gameOver == false        
+        // If game is not over, move to the left
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // If object goes off screen that is NOT the background, destroy it
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ cpe40032-c3-4/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+    public bool isLowEnough;
+    private float yBound = 12;
+
+    public AudioClip bounceSound;
+    public float bounceForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        // Problem 1 (The balloon should float up as the player presses spacebar)
+        // Solution: add playerRb = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && isLowEnough && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        // Problem 6 Bonus (Prevent the player from floating their balloon too high)
+        // Solution: add boolean isLowEnough and yBound.
+        // If the y position of the player is less than the ybound, set isLowEnough = false
+        if (transform.position.y > yBound)
+        {
+            isLowEnough = false;
+        }
+        // If the y position of the player is greater than the ybound, set isLowEnough = true
+        else
+        {
+            isLowEnough = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+        // Problem 9 Bonus (Make the balloon appear to bounce off of the ground, preventing it from leaving the bottom of the screen. There should be a sound effect when this happens, too!)
+        // Solution: add a tag "Ground" to the ground gameobject
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            // Adds a small upward force to let the player bounce
+            playerRb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.5f);
+        }
+    }
+}
